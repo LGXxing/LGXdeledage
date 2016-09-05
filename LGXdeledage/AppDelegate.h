c868272812794b1155e760d2ff12201a635d9171
刘广兴 <546589732@qq.com>
2016-09-05 09:26:18 +0800

    Initial Commit

---
+++ LGXdeledage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGXdeledage
+//
+//  Created by 兴哥哥 on 16/9/5.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LGXdeledage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LGXdeledage
+//
+//  Created by 兴哥哥 on 16/9/5.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
